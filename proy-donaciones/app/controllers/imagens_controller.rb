43dde0e74e344aded940af68efbad527775f4183
dafp18 <dafp18@hotmail.com>
2018-10-04 00:26:21 -0500

    CRUD imagenes
    se crea un crud para el manejo de las imagenes.

--- proy-donaciones/app/controllers/imagens_controller.rb
+++ proy-donaciones/app/controllers/imagens_controller.rb
@@ -1,7 +1,51 @@
 class ImagensController < ApplicationController
 
-def new
+    #usamos un callback para no repetir la misma linea en cada metodo.
 
+    before_action :enviar_imagen, only: [:show, :edit, :update, :destroy]
+
+    def index
+        @imagen=Imagen.all
+    end    
+
+def new
+@imagen=Imagen.new
+end
+
+def create
+#render plain: params[:imagen].inspect
+@imagen = Imagen.new imagen_params
+@imagen.save
+
+redirect_to @imagen
+end
+
+def show
+    
+end    
+
+def edit
+
+end
+
+def update
+    @imagen.update imagen_params
+    redirect_to @imagen
+    
+end
+
+def destroy
+    @imagen.destroy
+    redirect_to imagens_path
+    end
+
+    def enviar_imagen
+        @imagen = Imagen.find params[:id]
+    end  
+
+    private
+def imagen_params
+    params.require(:imagen).permit(:descripcion)
 end
 
 end
