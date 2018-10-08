0419b11b4233cdd5a7bf5dbe6505ae0a07308c25
Diego Forero <dafp18@hotmail.com>
2018-10-08 17:09:03 -0500

    se agrega la gema devise
    se configura la gema devise, se genera un modelo de usuarios, un controlador y una vista para devise, se verifica el funcionmiento y este registra los usuarios correctamente.

--- proy-donaciones/config/routes.rb
+++ proy-donaciones/config/routes.rb
@@ -1,5 +1,11 @@
 Rails.application.routes.draw do
+  devise_for :usuarios
+  get 'home/index'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  
+  root to: "home#index"
   resources :imagens
+
+  
 
 end
