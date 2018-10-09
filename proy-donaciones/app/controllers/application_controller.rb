8190b37d603d81502be2221c253b190f1d280ade
Diego Forero <dafp18@hotmail.com>
2018-10-09 11:53:53 -0500

    Agregamos el campo nombre
    se agrega el campo nombre a la base de datos, se agrega el text field a la vista sign_in, y se configuran los parametros de devise para indicarle que datos recibe para guardar en la base de datos, esto se modifca en el application_controller.rb

--- proy-donaciones/app/controllers/application_controller.rb
+++ proy-donaciones/app/controllers/application_controller.rb
@@ -1,2 +1,9 @@
 class ApplicationController < ActionController::Base
+    before_action :config_devise_param, if: :devise_controller?
+
+    def config_devise_param
+        devise_parameter_sanitizer.permit(:sign_up) do |user|
+            user.permit(:nombre, :email, :password, :password_confirmation)
+        end    
+    end    
 end
--- proy-donaciones/db/migrate/20181009163555_addcamponombreausuario.rb
+++ proy-donaciones/db/migrate/20181009163555_addcamponombreausuario.rb
@@ -1,4 +1,5 @@
 class Addcamponombreausuario < ActiveRecord::Migration[5.2]
   def change
+    add_column :usuarios, :nombre, :string
   end
 end
